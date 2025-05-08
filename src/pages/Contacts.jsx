75dd252a272c80ffd4e27fc60d3333c518851b40
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-05-08 16:50:12 +0530

    Done with front End

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import {assets} from '../assets/assets'
+import Tittle from '../components/Tittle'
+import Newsletter from '../components/NewsLetter'
 export default function Contacts() {
     return (
         <div>
-        <h1>dfsddsdssfd</h1>
+            <div className="text-center text-2xl pt-10 border-t">
+                <Tittle text1={'Contact'} text2={'US'}/> 
+            </div>
+
+            <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+                <img src={assets.contact_img} alt="" className="w-full md:max-w-[480px]" />
+                <div className="flex flex-col justify-center items-start gap-6">
+                    <p className="font-semibold text-xl text-gray-600">Our Store</p>
+                    <p className="text-gray-500">54709 Willms Station <br /> Suite 350, Washingstom DC</p>
+                    <p className="text-gray-500">tel:(451) 555-=1321 <br /> Email: admin@forever.com</p>
+                    <p className="font-semibold text-xl text-gray-600">Careers At Forever</p>
+                    <p className="text-gray-500">Learn more about out team and job opening </p>
+                    <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+                        Explore Jobs
+                    </button>
+                </div>
+            </div>
+                <Newsletter/>
+
         </div>
     )
 }
